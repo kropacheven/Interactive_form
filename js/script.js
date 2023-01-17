4089a1bed3f8acd213bd11abe669717fa3f2be04
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-01-17 12:24:14 +0300

    Correct name input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
 // Giving a name input field the focus state right when the page loads:
-const name = document.getElementById('name');
-name.focus();
+const inputName = document.getElementById('name');
+inputName.focus();
+
