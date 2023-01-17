725967cbf2f2f22f1095e1390582bebf99148ad6
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-01-17 13:02:19 +0300

    Job selection menu - other hide/unhide

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 
-// Giving a name input field the focus state right when the page loads:
+// 1. Giving a name input field the focus state right when the page loads:
 const inputName = document.getElementById('name');
 inputName.focus();
 
 
+// 2. Other Job role hide/unhide on Job selection menu:
+const otherJob = document.getElementById('other-job-role');
+otherJob.style.display = 'none';
 
+const jobRole = document.getElementById('title');
+const jobRoles = jobRole.querySelectorAll('option');
+console.log(jobRoles)
+
+for (let i = 0; i < jobRoles.length; i++) {
+    jobRole.addEventListener('change', () => {
+        if (jobRoles[i].value === 'other') {
+            otherJob.style.display = 'block';
+        } else {
+            otherJob.style.display = 'none';
+        }
+    });
+    //console.log(jobRoles[i].value);
+}
