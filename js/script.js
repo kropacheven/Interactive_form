64365fe2089c9790657d1d8d288d9b7cc754d528
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-01-18 19:21:55 +0300

    Made T-shirt selection section work

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ otherJob.style.display = 'none';
 
 const jobRole = document.getElementById('title');
 const jobRoles = jobRole.querySelectorAll('option');
-console.log(jobRoles)
+//console.log(jobRoles)
 
 for (let i = 0; i < jobRoles.length; i++) {
     jobRole.addEventListener('change', () => {
@@ -22,3 +22,34 @@ for (let i = 0; i < jobRoles.length; i++) {
     });
     //console.log(jobRoles[i].value);
 }
+
+// 3. T-shirt info section - selection menues:
+const design = document.getElementById('design'); // design root of selection menu - element;
+
+const color = document.getElementById('color');
+color.disabled = true;
+
+const options = document.querySelectorAll('#color option');
+//console.log(options[4]);
+
+
+design.addEventListener('change', (event) => {
+    color.disabled = false;
+    //for (let i = 0; i < options.length; i++) {
+    if (event.target.value === 'js puns' ) {
+        options[1].disabled = false;
+        options[2].disabled = false;
+        options[3].disabled = false;
+        options[4].disabled = true;
+        options[5].disabled = true;
+        options[6].disabled = true;
+    } else {
+        options[1].disabled = true;
+        options[2].disabled = true;
+        options[3].disabled = true;
+        options[4].disabled = false;
+        options[5].disabled = false;
+        options[6].disabled = false;
+    }
+    //}
+});
